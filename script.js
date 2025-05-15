3cfe5f7a19d5975c00bff55bfe15b430216b0270
Mvinaykumar007 <mprabhakarr21@gmail.com>
2025-05-15 08:22:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
+function numberOfBooksRead(library) {
+  // Check if library is an array
+  if (!Array.isArray(library)) {
+    throw new Error("Input must be an array of books.");
+  }
+
+  // Count books with readingStatus set to true
+  let count = 0;
+  for (let book of library) {
+    if (book.readingStatus === true) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+// Example usage:
 const library = [
   {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
+    author: 'Bill Gates',
+    title: 'The Road Ahead',
+    readingStatus: true
   },
   {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
+    author: 'Steve Jobs',
+    title: 'Walter Isaacson',
+    readingStatus: true
   },
   {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
+    author: 'Suzanne Collins',
+    title: 'Mockingjay: The Final Book of The Hunger Games',
+    readingStatus: false
+  }
 ];
 
-const numberOfBooksRead = () => {
-  // write your code here
-};
-
-// Do not change the code below
+console.log(numberOfBooksRead(library)); // Output: 2
